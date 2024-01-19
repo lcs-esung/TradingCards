961c17b9a4ba70e9896476d64a6a87b3100f04e1
Edna Sung <esung@lcs.on.ca>
2024-01-19 09:42:15 -0500

    Changed cards from Pokemon to Valorant. Added image to the phone.

--- TradingCards/Views/DetailView.swift
+++ TradingCards/Views/DetailView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 struct DetailView: View {
    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("JettImage")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding()
+                
+            
+        }
     }
 }
 
