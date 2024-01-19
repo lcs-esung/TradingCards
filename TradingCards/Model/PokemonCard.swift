421d4efa2acfa01811f35337ec785ffcf83adc06
Edna Sung <esung@lcs.on.ca>
2024-01-19 09:09:09 -0500

    Added different files.

---
+++ TradingCards/Model/PokemonCard.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonCard.swift
+//  TradingCards
+//
+//  Created by Edna Sung on 2024-01-19.
+//
+
+import SwiftUI
+
+struct PokemonCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonCard()
+}
---
+++ TradingCards/Views/DetailView.swift
@@ -0,0 +1,19 @@
+//
+//  DetailView.swift
+//  TradingCards
+//
+//  Created by Edna Sung on 2024-01-19.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+   
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
