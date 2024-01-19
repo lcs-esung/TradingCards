361fddf0b5a8731cbdc21f067f7a023d7da5dff5
Edna Sung <esung@lcs.on.ca>
2024-01-19 10:04:37 -0500

    Made image and background.

--- TradingCards/Views/DetailView.swift
+++ TradingCards/Views/DetailView.swift
@@ -11,12 +11,43 @@ struct DetailView: View {
    
     var body: some View {
         ZStack {
+            
+           Rectangle()
+                .foregroundColor(.black)
+                
+                .aspectRatio(1.0, contentMode: .fit)
+                .offset(y: -170)
+            
+            Rectangle()
+                 .foregroundColor(.white)
+                 .padding()
+                 .aspectRatio(1.0, contentMode: .fit)
+                 .offset(y: -170)
+                 .padding(-3)
+            
+            Rectangle()
+                 .foregroundColor(.red)
+                 .padding()
+                 .aspectRatio(1.0, contentMode: .fit)
+                 .offset(y: -170)
+                 .padding(8)
+            
+            Rectangle()
+                 .foregroundColor(.white)
+                 .padding()
+                 .aspectRatio(1.0, contentMode: .fit)
+                 .offset(y: -170)
+                 .padding(19)
+            
+            
             Image("JettImage")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .padding()
-                
+                .padding(29)
+                .offset(y: -170)
             
+            Image()
         }
     }
 }
