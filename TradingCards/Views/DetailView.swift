da417b17e6c2dffb388880f1e4b578224473d9e7
Edna Sung <esung@lcs.on.ca>
2024-01-21 17:34:47 -0500

    Added fonts to layout.

--- TradingCards/Views/DetailView.swift
+++ TradingCards/Views/DetailView.swift
@@ -13,34 +13,39 @@ struct DetailView: View {
         
         
         VStack {
+            
+            Text("Jett")
+                .font(Font.custom("Charter-Black", size: 40))
+                .padding(.leading, -170)
+                
+            
             ZStack {
                 
-               Rectangle()
-                    .foregroundColor(.black)
-                    
+                Rectangle()
+                    .foregroundColor(.cyan)
                     .aspectRatio(1.0, contentMode: .fit)
                     .offset(y: -170)
                 
                 Rectangle()
-                     .foregroundColor(.white)
-                     .padding()
-                     .aspectRatio(1.0, contentMode: .fit)
-                     .offset(y: -170)
-                     .padding(-3)
+                    .foregroundColor(.white)
+                    .padding()
+                    .aspectRatio(1.0, contentMode: .fit)
+                    .offset(y: -170)
+                    .padding(-3)
                 
                 Rectangle()
-                     .foregroundColor(.red)
-                     .padding()
-                     .aspectRatio(1.0, contentMode: .fit)
-                     .offset(y: -170)
-                     .padding(8)
+                    .foregroundColor(.red)
+                    .padding()
+                    .aspectRatio(1.0, contentMode: .fit)
+                    .offset(y: -170)
+                    .padding(8)
                 
                 Rectangle()
-                     .foregroundColor(.white)
-                     .padding()
-                     .aspectRatio(1.0, contentMode: .fit)
-                     .offset(y: -170)
-                     .padding(19)
+                    .foregroundColor(.white)
+                    .padding()
+                    .aspectRatio(1.0, contentMode: .fit)
+                    .offset(y: -170)
+                    .padding(19)
                 
                 Image("JettImage")
                     .resizable()
@@ -48,13 +53,13 @@ struct DetailView: View {
                     .padding()
                     .padding(29)
                     .offset(y: -170)
-
+                
                 Image("DuelistImage")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .padding()
-                    .padding(126)
-                    .offset(x: 105, y: -40)
+                    .padding(115)
+                    .offset(x: 105, y: -38)
                 
             }
             .padding(.top, 150)
@@ -62,48 +67,59 @@ struct DetailView: View {
             VStack(alignment: .leading) {
                 
                 Text("Skills")
+                    .font(Font.custom("Charter-Black", size: 20))
                     .bold()
                     .padding(.bottom)
-                    .padding(.leading, 0)
+                    .padding(.leading, 10)
                 
                 HStack(spacing: 14) {
                     
                     VStack(alignment: .leading) {
                         Text(" 1st skill")
+                            .font(Font.custom("Charter-Bold", size: 17))
                             .bold()
                         Text(" Updraft")
+                            .font(Font.custom("Charter-italic", size: 17))
                     }
                     
                     VStack(alignment: .leading) {
                         Text(" 2nd skill")
+                            .font(Font.custom("Charter-Bold", size: 17))
                             .bold()
                         Text(" Tailwind")
+                            .font(Font.custom("Charter-italic", size: 17))
                     }
                     
                     VStack(alignment: .leading) {
                         Text(" 3rd skill")
+                            .font(Font.custom("Charter-Bold", size: 17))
                             .bold()
                         Text(" Cloudburst")
+                            .font(Font.custom("Charter-italic", size: 17))
                     }
                     
                     VStack(alignment: .leading) {
                         Text(" Ultimate")
+                            .font(Font.custom("Charter-Bold", size: 17))
                             .bold()
                         Text(" Blade Storm")
+                            .font(Font.custom("Charter-italic", size: 17))
                     }
                     
-                   
+                    
                 }
             }
             .padding(.all, -150)
-        }
+            
             Text("Biography")
-            .bold()
-            .padding(.leading, -180)
-        
-        Text( "Representing her home country of South Korea, Jett's agile and evasive fighting style lets her take risks no one else can. She runs circles around every skirmish, cutting enemies before they even know what hit them.")
-            .padding(.all)
-      
+                .font(Font.custom("Charter-Black", size: 20))
+                .bold()
+                .padding(.leading, -170)
+            
+            Text( "Representing her home country of South Korea, Jett's agile and evasive fighting style lets her take risks no one else can. She runs circles around every skirmish, cutting enemies before they even know what hit them.")
+                .font(Font.custom("Charter-italic", size: 17))
+                .padding(.all)
+        }
     }
         
 }
