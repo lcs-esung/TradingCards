1782b9300e429ab04a2a389070da950cf7343b4a
Edna Sung <esung@lcs.on.ca>
2024-01-21 14:07:10 -0500

    Added skills for agent.

--- TradingCards/Views/DetailView.swift
+++ TradingCards/Views/DetailView.swift
@@ -10,50 +10,84 @@ import SwiftUI
 struct DetailView: View {
    
     var body: some View {
-        ZStack {
-            
-           Rectangle()
-                .foregroundColor(.black)
+        
+        VStack {
+            ZStack {
                 
-                .aspectRatio(1.0, contentMode: .fit)
-                .offset(y: -170)
-            
-            Rectangle()
-                 .foregroundColor(.white)
-                 .padding()
-                 .aspectRatio(1.0, contentMode: .fit)
-                 .offset(y: -170)
-                 .padding(-3)
-            
-            Rectangle()
-                 .foregroundColor(.red)
-                 .padding()
-                 .aspectRatio(1.0, contentMode: .fit)
-                 .offset(y: -170)
-                 .padding(8)
-            
-            Rectangle()
-                 .foregroundColor(.white)
-                 .padding()
-                 .aspectRatio(1.0, contentMode: .fit)
-                 .offset(y: -170)
-                 .padding(19)
-            
-            Image("JettImage")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding()
-                .padding(29)
-                .offset(y: -170)
+               Rectangle()
+                    .foregroundColor(.black)
+                    
+                    .aspectRatio(1.0, contentMode: .fit)
+                    .offset(y: -170)
+                
+                Rectangle()
+                     .foregroundColor(.white)
+                     .padding()
+                     .aspectRatio(1.0, contentMode: .fit)
+                     .offset(y: -170)
+                     .padding(-3)
+                
+                Rectangle()
+                     .foregroundColor(.red)
+                     .padding()
+                     .aspectRatio(1.0, contentMode: .fit)
+                     .offset(y: -170)
+                     .padding(8)
+                
+                Rectangle()
+                     .foregroundColor(.white)
+                     .padding()
+                     .aspectRatio(1.0, contentMode: .fit)
+                     .offset(y: -170)
+                     .padding(19)
+                
+                Image("JettImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                    .padding(29)
+                    .offset(y: -170)
 
-            Image("DuelistImage")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding()
-                .padding(126)
-                .offset(x: 105, y: -40)
+                Image("DuelistImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                    .padding(126)
+                    .offset(x: 105, y: -40)
+                
+            }
+            .padding(.top, 80)
             
+            VStack(alignment: .leading) {
+                
+                Text("Skills")
+                    .bold()
+                    .padding(.bottom)
+                
+                HStack(spacing: 20) {
+                    
+                    VStack(alignment: .leading) {
+                        Text(" 1st skill")
+                        Text(" Updraft")
+                        
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text(" 2nd skill")
+                        Text(" Tailwind")
+                        
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text(" 3rd skill")
+                        Text(" Cloudburst")
+                        
+                    }
+                }
+            }
         }
+        
+      
     }
 }
 
