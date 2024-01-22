80ae9ff6093257d9b4a2a67aaab52ac0a0d804a4
Edna Sung <esung@lcs.on.ca>
2024-01-22 18:25:23 -0500

    Fixed spacing and final touches to the crown.

--- TradingCards/Views/DetailView.swift
+++ TradingCards/Views/DetailView.swift
@@ -45,8 +45,6 @@ struct DetailView: View {
                             .padding()
                             
                         
-
-                        
                         VStack {
                             HStack {
                                 
@@ -70,129 +68,126 @@ struct DetailView: View {
                     .padding(.top)
                     
                     
-                    ZStack {
-                        
-                        Image("Banner")
-                            .resizable()
-                           
+                    
+                    
+                        ZStack {
+                            
+                            
+                            VStack {
+                                Spacer()
+                                    .padding()
+                                    
+                                Image("JettCrown")
+                                    .resizable()
+                                .scaledToFit()
+                            }
+                            
+                            VStack {
+                                Spacer()
+                                    .padding()
+                                    .padding()
+                                    .padding()
+                                    .padding()
                                 
-                                VStack {
-                                    Rectangle()
+                                Image("Banner")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    
+                                
+                            }
+
+                            VStack {
+                                Rectangle()
                                         .foregroundColor(.white)
                                         .aspectRatio(contentMode: .fit)
-                                        .padding()
-                                    
-                                    Spacer()
-                                }
-                                    .overlay(
-                                        
-                                        
-                                        VStack(alignment: .leading) {
-                                            
-                                            Spacer()
-                                            Spacer()
-                                            
-                                            
-                                            HStack {
-                                                
-                                                Spacer(minLength: 30)
-                                                
-                                                Text("Skills")
-                                                    .font(Font.custom("Charter-Black", size: 20))
-                                                    .bold()
-                                                    .padding(.bottom)
-                                                    .padding(.top)
-                                                
-                                                Spacer()
-                                                    .padding()
-                                            }
-                                            
-                                            
-                                            HStack(spacing: 12) {
-                                                
-                                                VStack(alignment: .leading) {
-                                                    Text(" 1st skill")
-                                                        .font(Font.custom("Charter-Bold", size: 17))
-                                                        .bold()
-                                                    Text(" Updraft")
-                                                        .font(Font.custom("Charter-italic", size: 17))
-                                                }
-                                                
-                                                VStack(alignment: .leading) {
-                                                    Text(" 2nd skill")
-                                                        .font(Font.custom("Charter-Bold", size: 17))
-                                                        .bold()
-                                                    Text(" Tailwind")
-                                                        .font(Font.custom("Charter-italic", size: 17))
-                                                }
-                                                
-                                                VStack(alignment: .leading) {
-                                                    Text(" 3rd skill")
-                                                        .font(Font.custom("Charter-Bold", size: 17))
-                                                        .bold()
-                                                    Text(" Cloudburst")
-                                                        .font(Font.custom("Charter-italic", size: 17))
-                                                }
-                                                
-                                                VStack(alignment: .leading) {
-                                                    Text(" Ultimate")
-                                                        .font(Font.custom("Charter-Bold", size: 17))
-                                                        .bold()
-                                                    Text(" Blade Storm")
-                                                        .font(Font.custom("Charter-italic", size: 17))
-                                                }
-                                                
-                                            }
-                                            
-                                            
-                                            .padding(.leading, 20)
-                                            
-                                            VStack {
-                                                HStack {
+                                    .padding()
+                                Spacer()
+                                 
+                            }
+                        
+                                    VStack(alignment: .leading) {
+                                       
+                                        HStack {
                                                     
+                                            Spacer(minLength: 25)
                                                     
-                                                    Text("Biography")
+                                                    Text("Skills")
                                                         .font(Font.custom("Charter-Black", size: 20))
                                                         .bold()
-                                                        .padding(.leading, 25)
-                                                        .padding(.top)
                                                         .padding(.bottom)
+                                                        .padding(.top, 40)
                                                     
                                                     Spacer()
+                                                        .padding()
                                                 }
+                                                
+                                                
+                                                HStack(spacing: 12) {
+                                                    
+                                                    VStack(alignment: .leading) {
+                                                        Text(" 1st skill")
+                                                            .font(Font.custom("Charter-Bold", size: 17))
+                                                            .bold()
+                                                        Text(" Updraft")
+                                                            .font(Font.custom("Charter-italic", size: 17))
+                                                    }
+                                                    
+                                                    VStack(alignment: .leading) {
+                                                        Text(" 2nd skill")
+                                                            .font(Font.custom("Charter-Bold", size: 17))
+                                                            .bold()
+                                                        Text(" Tailwind")
+                                                            .font(Font.custom("Charter-italic", size: 17))
+                                                    }
+                                                    
+                                                    VStack(alignment: .leading) {
+                                                        Text(" 3rd skill")
+                                                            .font(Font.custom("Charter-Bold", size: 17))
+                                                            .bold()
+                                                        Text(" Cloudburst")
+                                                            .font(Font.custom("Charter-italic", size: 17))
+                                                    }
+                                                    
+                                                    VStack(alignment: .leading) {
+                                                        Text(" Ultimate")
+                                                            .font(Font.custom("Charter-Bold", size: 17))
+                                                            .bold()
+                                                        Text(" Blade Storm")
+                                                            .font(Font.custom("Charter-italic", size: 17))
+                                                    }
+                                                    
+                                                }
+                                                
+                                                
+                                                .padding(.leading, 20)
+                                                
+                                        VStack {
+                                            HStack {
+                                                Text("Biography")
+                                                    .font(Font.custom("Charter-Black", size: 20))
+                                                    .bold()
+                                                    .padding(.leading, 25)
+                                                    .padding(.top)
+                                                    .padding(.bottom)
+                                                
+                                                Spacer()
                                             }
-                                            
-                                            
+                                          
                                             
                                             Text( "Representing her home country of South Korea, Jett's agile and evasive fighting style lets her take risks no one else can. She runs circles around every skirmish, cutting enemies before they even know what hit them.")
                                                 .font(Font.custom("Charter-italic", size: 17))
                                                 .padding(.leading, 20)
                                             
-                                            
-                                            Spacer()
-                                            Spacer()
-                                                .padding()
+                                         
                                         }
-                                    )
+                                        
+                                        Spacer()
+                                }
                             
-                        
-                        
-                        
-                        
-                        VStack {
-                            Spacer()
-                            
-                            Image("JettCrown")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .padding()
-                                .padding()
-                            
-                            Spacer()
                         }
-                    }
-                     
                     
+                  
+                 
                 }
                 .background(.black)
             }
